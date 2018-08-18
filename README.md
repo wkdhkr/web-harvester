617778b12da4c2d7fad68e409129074dce39b040
wicked hacker <33594992+wkdhkr@users.noreply.github.com>
2018-08-19 00:43:02 +0900

    add badge, remove needless rollup config.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Web Harvester
 
+[![Build status](https://ci.appveyor.com/api/projects/status/gu75vs1ot8gkiyd2?svg=true)](https://ci.appveyor.com/project/wkdhkr/web-harvester)
+[![Coverage Status](https://coveralls.io/repos/github/wkdhkr/web-harvester/badge.svg)](https://coveralls.io/github/wkdhkr/web-harvester)
+
 WIP
--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import sourceMaps from 'rollup-plugin-sourcemaps'
-import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
 
@@ -12,7 +10,6 @@ const libraryName = 'web-harvester'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -31,8 +28,5 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
-
-    // Resolve source maps to the original source
-    sourceMaps(),
   ],
 }
